0f6fecf32e34e11074d4ba47a2e2992cac46b285
roaa16-meet <roaa16@meet.mit.edu>
2015-11-05 21:10:32 +0200

    flask

---
+++ flaskp.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+	return render_template("homepage.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
+
+
+
+if __name__ == "__main__": 
+	app.run() 
